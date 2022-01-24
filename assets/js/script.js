80c805283d58c054fefacf36ab687b17f2c352c8
Christian Donoso <chrst.donoso@gmail.com>
2022-01-24 17:23:08 -0300

    proyecto con efectos agregados en navbar, tooltips y alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
 
+// js para tooltips bootstrap 
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 
 // CAMBIA EL COLOR DE LA NAVBAR CON EL SCROLL 
-
 $(window).scroll(function() {
-    if ($("#menu").offset().top > 56) {
+    if ($("#menu").offset().top > 120) {
+        $("#menu").removeClass("bg-transparent");
         $("#menu").addClass("bg-dark");
     } else {
         $("#menu").removeClass("bg-dark");
     }
 });
 
+// mensjae de correo enviado al hacer click en boton de envio.
+$(function(){
+    $("#enviar").on("click", function(){
+        alert("Correo electronico enviado con exito")
+    });
+});
